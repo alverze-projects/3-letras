cf6e3c914511630e0463edc9c6d42a444eaf00a1
Luis Arriaza <aluis@alvaci.com>
2026-03-05 16:07:08 -0600

    feat: Implement dictionary loading from a database using TypeORM and add a reload mechanism.

--- apps/api/src/dictionary/dictionary.module.ts
+++ apps/api/src/dictionary/dictionary.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VocabEntry } from '../entities/vocab-entry.entity';
 import { DictionaryService } from './dictionary.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([VocabEntry])],
   providers: [DictionaryService],
   exports: [DictionaryService],
 })
-export class DictionaryModule {}
+export class DictionaryModule { }
